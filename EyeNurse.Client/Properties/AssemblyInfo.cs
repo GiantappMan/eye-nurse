4e4e0878b364eed9f2884b696517d20b3ef1193b
DaZiYuan <mscoder@hotmail.com>
2018-07-27 11:31:48 +0800

    1.0.0.30

--- EyeNurse.Client/Properties/AssemblyInfo.cs
+++ EyeNurse.Client/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.25")]
-[assembly: AssemblyFileVersion("1.0.0.25")]
-[assembly: AssemblyInformationalVersion("1.0.0.25-eec0beb")]
+[assembly: AssemblyVersion("1.0.0.30")]
+[assembly: AssemblyFileVersion("1.0.0.30")]
+[assembly: AssemblyInformationalVersion("1.0.0.30-762c619")]
